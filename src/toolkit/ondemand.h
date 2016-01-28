d697f9f86ba2b7b1fd0a4cb4c6d97685d0b1563d
Robert Carlsen <rwcarlsen@gmail.com>
2016-01-28 13:17:13 -0600

    fix compile errors and get test working

--- src/toolkit/ondemand.h
+++ src/toolkit/ondemand.h
@@ -8,7 +8,7 @@ namespace toolkit {
 
 class Ondemand {
  public:
-  Ondemand() : usage_buf_frac_(1.5), max_(false), window_(12), empty_thresh_(1e-6) {};
+  Ondemand() : qty_used_(nullptr), usage_buf_frac_(2.0), max_(false), window_(12), empty_thresh_(1e-6) {};
   virtual ~Ondemand() {};
 
   Ondemand& Init(std::list<double>* qty_used, double usage_guess) {
@@ -18,19 +18,19 @@ class Ondemand {
       qty_used_->push_back(usage_guess);
       qty_used_->push_back(0);
     }
+    return *this;
   };
     
   // the total amount to request or fill in for the time step
-  double ToMove() {
-    double curr_qty = qty_used_->back();
+  double ToMove(double curr_qty) {
     return std::max(0.0, MovingUsage() * usage_buf_frac_ - curr_qty);
   }
 
-  void use_max() {max_ = true;};
-  void use_avg() {max_ = false;};
-  void usage_buf_frac(double frac) {usage_buf_frac_ = frac;};
-  void empty_thresh(double qty) {empty_thresh_ = qty;};
-  void window(int width) {window_ = width;};
+  Ondemand& use_max() {max_ = true; return *this;};
+  Ondemand& use_avg() {max_ = false; return *this;};
+  Ondemand& usage_buf_frac(double frac) {usage_buf_frac_ = frac; return *this;};
+  Ondemand& empty_thresh(double qty) {empty_thresh_ = qty; return *this;};
+  Ondemand& window(int width) {window_ = width; return *this;};
 
   double MovingUsage() {
     if (qty_used_->size() < 2) {
@@ -40,41 +40,45 @@ class Ondemand {
     std::list<double>::iterator it;
     if (max_) {
       double max = 0;
-      for (it = qty_used_->begin(); it + 1 != qty_used_->end(); ++it) {
-        if (*it > max) {
-          max = *it;
+      for (it = qty_used_->begin(); it != qty_used_->end(); ) {
+        double val = *it;
+        ++it;
+        if (it == qty_used_->end()) break;
+
+        if (val > max) {
+          max = val;
         }
       }
       return max;
     } else { // avg
       double tot = 0;
-      for (it = qty_used_->begin(); it + 1 != qty_used_->end(); ++it) {
-        tot += *it;
+      for (it = qty_used_->begin(); it != qty_used_->end(); ) {
+        double val = *it;
+        ++it;
+        if (it == qty_used_->end()) break;
+        tot += val;
       }
-      return tot / std::max(1e-100, qty_used_->size() - 1);
+      return tot / std::max(1e-100, qty_used_->size() - 1.00);
     }
   }
-
-  void UpdateUsage(double qty_available) {
+  
+  void UpdateUsage(double pre_use_qty, double post_use_qty) {
     double prev_qty = qty_used_->back();
-    double used = 0.0
-    if (prev_qty < empty_thresh_) {
+    double used = pre_use_qty - post_use_qty; 
+    if (pre_use_qty < empty_thresh_) {
       // demand was probably larger than available inventory so assume
       // demand/virtual-usage is equal to usage plus safety buffer
       used = MovingUsage() * usage_buf_frac_;
-    } else {
-      used = qty_available - prev_qty;
     }
 
     qty_used_->pop_back();
     qty_used_->push_back(used);
-    qty_used_->push_back(qty_available);
+    qty_used_->push_back(post_use_qty);
 
     if (qty_used_->size() > window_) {
       qty_used_->pop_front();
     }
   }
-
 
  private:
   double empty_thresh_;
--- tests/toolkit/ondemand_tests.cc
+++ tests/toolkit/ondemand_tests.cc
@@ -6,29 +6,37 @@
 namespace cyclus {
 namespace toolkit {
 
-TEST(OndemandTests, ScaleUp_NetZero) {
-
-  std::vector<double> recv_history = {30, 30, 30, 30};
-  std::vector<double> send_history = {0, 30, 30, 30};
-  std::vector<double> want_to_move = {0, 0, 0, 0};
-
-  double usage_guess = 3;
+void CheckBuy(double usage_guess, int window, double frac, std::vector<double> recv, std::vector<double> used, std::vector<double> want) {
   std::list<double> qty_used;
-
   Ondemand od;
-  od.Init(&qty_used, usage_guess);
-  check(od, recv_history, send_history, want_to_move);
+  od.Init(&qty_used, usage_guess).window(window).usage_buf_frac(frac);
 
+  double qty = 0.0;
+  double surplus = 0;
+  double deficit = 0;
+  for (int i = 0; i < recv.size(); i++) {
+    double touse = std::min(qty, used[i] + deficit);
+    deficit += used[i] - touse;
+    od.UpdateUsage(qty, qty - touse);
+    qty -= touse;
+    //EXPECT_DOUBLE_EQ(want[i], od.ToMove(qty));
+    double tomove = std::min(recv[i] + surplus, od.ToMove(qty));
+    qty += tomove;
+    surplus += recv[i] - tomove;
+    std::cout << "- qty0=" << qty+touse-tomove << ", qty1=" << qty-tomove << ", qty2=" << qty << "\n";
+    std::cout << "  moved " << tomove << "/" << od.ToMove(qty-tomove) << ", used " << touse << ", surplus=" << surplus << ", deficit=" << deficit << "\n";
+  }
 }
 
-void check(Ondemand& od, std::vector<double> recv, std::vector<double> send, std::vector<double> want) {
-  double qty = 0.0;
-  for (int i = 0; i < recv_history.size(); i++) {
-    qty += recv[i] - send[i];
-    od.UpdateUsage(qty);
-    EXPECT_DOUBLE_EQ(want[i], od.ToMove());
-  }
+TEST(OndemandTests, ScaleUp_NetZero) {
+  std::vector<double> recv_history = {30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30};
+  std::vector<double> send_history = {0, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30};
+  std::vector<double> want_to_move = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+  double usage_guess = 3;
+  int window = 12;
+  double frac = 2.0;
 
+  CheckBuy(usage_guess, window, frac, recv_history, send_history, want_to_move);
 }
 
 }
